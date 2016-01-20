4753d9ad3144220e34e8c1eed28c25cd9a6f5598
Elvisthacoder <ekk213@gmail.com>
2016-01-20 12:52:57 +0300

    sequence

---
+++ sequence/sequence.java
@@ -0,0 +1,30 @@
+package sequence;
+
+import java.util.*;
+
+public class sequence {
+
+	public int [] sequence(int a, int b, int c) {
+		
+	
+	int min  = Math.min(Math.min(a, b),c);
+	int max  = Math.max(Math.min(a, b),c);
+	int[] num={a,b,c};
+	int[] numbers = 0;
+	Arrays.sort(numbers);
+	System.out.println(Arrays.toString(numbers));
+	return numbers;
+	
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		sequence num = new sequence();
+		 num.sequence(4, 8, 10);
+		 num.sequence(4, 14, 10);
+		 
+
+	}
+
+}
